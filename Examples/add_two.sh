22441f053218d17077e556ed32bfc7f0840295ed
Matt Gitzendanner <magitz@ufl.edu>
2020-09-29 08:50:50 -0400

    Fix typo

--- Examples/add_two.sh
+++ Examples/add_two.sh
@@ -5,7 +5,7 @@
 add_two () {
    # Take 2 numbers and add them, returning the sum
 
-   num1=$1 # The first argument is $1, assing to num1 variable
+   num1=$1 # The first argument is $1, assign to num1 variable
    num2=$2 # The second argument is $2, assign to num2 variable 
    
    sum=$((num1 + num2))  # Add the two numbers
